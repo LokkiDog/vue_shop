15ceda4852586b42877208c74fbeda39acdd8ebc
IzvekovWeb <izvekov.web@yandex.ru>
2020-01-07 14:21:22 +0300

    Настроены routes. Начало Store

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,44 @@
 
     <!-- header -->
 
-    <div class="wrapper-content">
-      <section>
-        <div class="container"> 
-          <h1>Hello</h1>
-          <img alt="Vue logo" src="./assets/logo.png"> 
+    <header>
+      <div class="navbar">
+        <div class="container">
+          <div class="navbar-content">
+            <div class="logo">VUE-CLI</div>
+            <ul class="navbar-list">
+              <li class="navbar-item" v-for="link in links" :key="link.title">
+                <router-link class="navbar-link" :title="link.title" :to="link.url">{{ link.title }}</router-link>
+              </li> 
+            </ul>
+          </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </header>
 
+    <router-view></router-view>
      <!-- footer -->
     
   </div>
 </template>
 
 <script> 
-export default {} 
+export default {
+  data() {
+    return {
+      links: [
+        {title: 'Home', url: '/'},
+        {title: 'Example', url: '/example'},
+      ]
+    }
+  }
+} 
 </script> 
+
+<style lang="scss">
+.navbar-link {
+  &.router-link-exact-active {
+    color: #5247e7;
+  }
+}
+</style>
