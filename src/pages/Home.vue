8e60a7056c994efa065004d940852b5bc5f19d45
IzvekovWeb <izvekov.web@yandex.ru>
2020-01-07 15:20:13 +0300

    Модульность Store

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,19 +2,9 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container"> 
-        <h1>HOME Page</h1> 
-        <p>{{ message }}</p>
+        <h1>HOME Page</h1>  
       </div>
     </section>
   </div>
 </template>
-
-<script>
-export default { 
-  computed: {
-    message () {
-      return this.$store.getters.getMessage
-    }
-  }
-}
-</script>
+ 
